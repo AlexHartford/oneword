6c27a6923e0c63a2bd8e06adf91ff729ed27787e
Alex Hartford <alex.freshtech@gmail.com>
2020-04-06 10:03:16 -0700

    Got kebab to have less padding

--- lib/src/tabs/home/kebab.dart
+++ lib/src/tabs/home/kebab.dart
@@ -8,43 +8,47 @@ class Kebab extends HookWidget {
   Widget build(BuildContext context) {
     var _selection = useState(Item.add);
 
-    return PopupMenuButton<Item>(
-      onSelected: (Item result) {
-        _selection.value = result;
-      },
-      padding: EdgeInsets.all(0),
-      itemBuilder: (BuildContext context) => <PopupMenuEntry<Item>>[
-        PopupMenuItem<Item>(
-          value: Item.add,
-          child: Row(
-            children: <Widget>[
-              Icon(Icons.person_add),
-              SizedBox(width: 5),
-              Text('Add user'),
-            ],
+    return SizedBox(
+      height: 25,
+      width: 15,
+      child: PopupMenuButton<Item>(
+        onSelected: (Item result) {
+          _selection.value = result;
+        },
+        padding: EdgeInsets.all(0),
+        itemBuilder: (BuildContext context) => <PopupMenuEntry<Item>>[
+          PopupMenuItem<Item>(
+            value: Item.add,
+            child: Row(
+              children: <Widget>[
+                Icon(Icons.person_add),
+                SizedBox(width: 5),
+                Text('Add user'),
+              ],
+            ),
           ),
-        ),
-        PopupMenuItem<Item>(
-          value: Item.save,
-          child: Row(
-            children: <Widget>[
-              Icon(Icons.save_alt),
-              SizedBox(width: 5),
-              Text('Save'),
-            ],
+          PopupMenuItem<Item>(
+            value: Item.save,
+            child: Row(
+              children: <Widget>[
+                Icon(Icons.save_alt),
+                SizedBox(width: 5),
+                Text('Save'),
+              ],
+            ),
           ),
-        ),
-        PopupMenuItem<Item>(
-          value: Item.report,
-          child: Row(
-            children: <Widget>[
-              Icon(Icons.warning),
-              SizedBox(width: 5),
-              Text('Report'),
-            ],
+          PopupMenuItem<Item>(
+            value: Item.report,
+            child: Row(
+              children: <Widget>[
+                Icon(Icons.warning),
+                SizedBox(width: 5),
+                Text('Report'),
+              ],
+            ),
           ),
-        ),
-      ],
+        ],
+      ),
     );
   }
 }
