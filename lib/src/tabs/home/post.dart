ae42334c5719850c8d45c0671fcd5715cc11ab76
Alex Hartford <alex.freshtech@gmail.com>
2020-04-06 10:39:02 -0700

    removed post transform, instead set card margin to zero and made a straight border.  that way it works in landscape view

--- lib/src/tabs/home/post.dart
+++ lib/src/tabs/home/post.dart
@@ -1,25 +1,23 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_hooks/flutter_hooks.dart';
+
 import 'package:oneword/src/tabs/home/buttons.dart';
 import 'package:oneword/src/tabs/home/content.dart';
-import 'package:oneword/src/tabs/home/kebab.dart';
 
 class Post extends StatelessWidget {
   Post({Key key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Transform.scale(
-      scale: 1.05,
-      child: Padding(
-        padding: const EdgeInsets.only(bottom: 8.0),
-        child: Card(
-          child: Column(
-            children: <Widget>[
-              Content(),
-              Buttons()
-            ],
-          ),
+    return Padding(
+      padding: const EdgeInsets.only(bottom: 8.0),
+      child: Card(
+        margin: const EdgeInsets.all(0),
+        shape: Border(),
+        child: Column(
+          children: <Widget>[
+            Content(),
+            Buttons()
+          ],
         ),
       ),
     );
